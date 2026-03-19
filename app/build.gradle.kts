66579305970338cdf36bd5235901f21993ea2d62
flowerswithoppo <flowerswithoppo@gmail.com>
2026-03-19 13:18:10 +0300

    Bump compile/target SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.wourd.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.wourd.app"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
